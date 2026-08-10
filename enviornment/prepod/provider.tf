389cbb2fef3b88be26099e80705ed348bd4191a3
deepak-devops-max <deepakkoushal393@gmail.com>
2026-08-10 15:37:05 +0530

    feat: configure azurerm provider and backend state for prepod and prod environments

--- enviornment/prepod/provider.tf
+++ enviornment/prepod/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-devo"
+    storage_account_name = "storageaccount671"
+    container_name       = "tfstate"
+    key                  = "prepod.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
