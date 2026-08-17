d2705341499d4c73e4d2f58f3ffeca6cf5800677
deepak-devops-max <deepakkoushal393@gmail.com>
2026-08-17 10:41:15 +0530

    new-code1

--- enviornment/provider.tf
+++ enviornment/provider.tf
@@ -1,12 +1,7 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-devo"
-    storage_account_name = "storageaccount671"
-    container_name       = "tfstate"
-    key                  = "prepod.terraform.tfstate"
-  }
+ 
 
-  required_providers {
+ required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 5.0"
